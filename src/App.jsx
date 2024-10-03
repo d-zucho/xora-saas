ec320f76e837a16b35efa070bf6c21701f76a909
D_Zucho <danny.nico.dev@outlook.com>
2024-10-02 18:27:36 -0700

    add header - destop and mobile nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './sections/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className='text-base'>app</div>
-    </>
+    <main className='overflow-hidden relative'>
+      <Header />
+    </main>
   )
 }
 

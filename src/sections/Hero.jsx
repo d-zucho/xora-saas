03e8fd63afe047ef40b9fc34930fea2a6840b0a9
D_Zucho <danny.nico.dev@outlook.com>
2024-10-03 00:46:03 -0700

    create Button component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import { Element, Link as LinkScroll } from 'react-scroll'
+import Button from '../components/Button'
 
 const Hero = () => {
   return (
-    <div className='relative border-2 border-amber-400 pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 z-0'>
-      Hero
-    </div>
+    <section className='relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 z-0'>
+      <Element name='hero'>
+        <div className='container'>
+          <div className='relative z-2 max-w-512 max-lg:max-w-388'>
+            <div className='caption small-2 uppercase text-p3'>
+              Video Editing
+            </div>
+            <h1 className='mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12'>
+              Amazingly Simple
+            </h1>
+            <p className='max-w-440 mb-14 body-1 max-md:mb-10'>
+              We designed XORA AI Video Editor to be easy to use, quick to
+              learn, and suprisingly powerful.
+            </p>
+            <LinkScroll to='features' offset={-100}>
+              <Button icon='/images/zap.svg'>Try it now</Button>
+            </LinkScroll>
+          </div>
+        </div>
+      </Element>
+    </section>
   )
 }
 
